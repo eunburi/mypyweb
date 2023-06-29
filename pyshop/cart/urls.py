6ab07d3558d274fa252c88d9709a20e83408b9e7
eunburi <finestar23@naver.com>
2023-06-29 19:18:29 +0900

    pyshop

--- pyshop/cart/urls.py
+++ pyshop/cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from cart import views
 
-appname = 'cart'
+app_name = 'cart'
 
 urlpatterns = [
-    path('', views.detail, name='detail'),
-
+    path('', views.detail, name='detail'), # 장바구니 상세
+    path('add/<int:product_id>/', views.add,
+                    name='product_add'),   # 장바구니에 제품 추가
+    path('remove/<int:product_id>/', views.remove,
+                    name='product_remove') # 장바구니에 제품 삭제
 ]
-
